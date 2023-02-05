796064d52dc0461bd928450661a3e816ce51d1c4
Juhyeon Kim <woonikim@naver.com>
2023-02-05 19:58:05 +0900

    [ADD] CREATE PAGE

--- app.py
+++ app.py
@@ -1,43 +1,86 @@
-from flask import Flask, render_template
+import uuid
+from flask import Flask, Response, request, render_template
 from gensim.models import Word2Vec
 
 app = Flask(__name__)
 
-seed = ''
-history = []
-list_len = 0
-
+game_env = []
 model = Word2Vec.load('./models/word2vec-KCC150.model')
-
 
 @app.route('/', methods=['GET'])
 def index():
+    return render_template('create_game.html')
+
+
+@app.route('/', methods=['POST'])
+def create_game():
+    data = request.get_json()
+    seed = data['seed']
+    creator = data['creator']
+
+    try:
+        similars = model.wv.most_similar(positive=[seed], topn=500)
+        key = add_game_seed(seed, creator, similars)
+    
+    except:
+        return Response(
+            f"{seed}: 모르는 단어입니다.",
+            status=400
+        )
+
+    return {"key": key}
+
+
+def add_game_seed(seed, creator, similars):
+    global game_env
+
+    key = str(uuid.uuid4())
+    key = key.replace('-', '')
+
+    game_env.append({
+        "key": key,
+        "seed": seed,
+        "creator": creator,
+        "similars": similars
+    })
+
+    return key
+
+
+@app.route('/game/<key>', methods=['GET'])
+def game_page(key):
     return render_template('main.html')
+    
+
+@app.route('/similars/<key>', methods=['GET'])
+def get_similars(key):
+    global game_env
+
+    for data in game_env:
+        if data['key'] == key:
+            return data['similars']
+
+    return Response(status=400)
 
 
-@app.route('/seed/<word>')
-def set_seed(word):
-    global seed
-    global history
-    global list_len
+@app.route('/check/<key>/<word>')
+def check_similarity(key, word):
+    seed = None
+    for data in game_env:
+        if data.key == key:
+            seed = data.seed
 
-    history = []
-    list_len = 0
-    seed = word
+    if seed is None:
+        return Response(
+            {"message": "존재하지 않는 게임입니다."},
+            status=404
+        )
 
-    return {
-        "success": True
-    }
-
-
-@app.route('/check/<word>')
-def check_similarity(word):
     if seed == word:
         return {
             "success": True,
             "isAnswer": True,
-            "similarity": '100.0',
-            "history": build_history()
+            "similarity": '100.0'
         }
 
     try:
@@ -45,53 +88,15 @@ def check_similarity(word):
 
     except KeyError:
         return {
-            "success": False
+            "success": False,
+            "isAnswer": False
         }
-
-    add_data(word, similarity)
 
     return {
         "success": True,
         "isAnswer": False,
-        "similarity": str(similarity),
-        "history": build_history()
+        "similarity": str(similarity)
     }
-
-
-def add_data(word, similarity):
-    global history
-    global list_len
-
-    for data in history:
-        if data[2] == word:
-            return
-
-    count = list_len + 1
-    data = (similarity, count, word)
-
-    if list_len == 0:
-        history.append(data)
-    else:
-        found = False
-
-        for i in range(list_len):
-            if data > history[i]:
-                history.insert(i, data)
-                found = True
-                break
-
-        if found is False:
-            history.append(data)
-
-    list_len = list_len + 1
-
-
-def build_history():
-    text = ''
-    for data in history:
-        text = f"{text}#{data[1]}\t{data[2]}\t{data[0]}\n"
-
-    return text
 
 
 app.run(host='127.0.0.1', port=8080)
